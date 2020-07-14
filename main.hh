0050ee154c96a0802312bc7db92bbacb02fb7dc4
Darren Covey <48165454+darrencovey@users.noreply.github.com>
2020-07-14 14:20:49 -0300

    Initial git commit!

---
+++ main.hh
@@ -0,0 +1,8 @@
+#ifndef __20200714_987234987239847_main.hh__
+#define __20200714_987234987239847_main.hh__
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
+
+#endif // __20200714_987234987239847_main.hh__
